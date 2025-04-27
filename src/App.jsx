d92c4881017106b527db3fabbd3a50b6b9c840ce
chandraganesh96 <chandraganesh463@gmail.com>
2025-04-27 19:13:08 +0530

    Fix import path casing for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./CSS/App.css";
 import Favorites from "./Pages/Favorites";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import { Routes, Route } from "react-router-dom";
 import { MovieProvider } from "./Contexts/MovieContext";
 import NavBar from "./Components/NavBar";
